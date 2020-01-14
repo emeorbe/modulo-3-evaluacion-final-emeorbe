51bbe10d6a2809b561092aa96fbeea46e53e4773
emeorbe <meli.orbe@gmail.com>
2020-01-14 19:32:03 +0100

    generate docs

---
+++ docs/precache-manifest.6c2c7b72b620bcd838832a64d3de065c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91da2750f8bea83f919b3b393ec2b7ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30427ed1fc08df9f18bc",
+    "url": "./static/css/main.0e7e3277.chunk.css"
+  },
+  {
+    "revision": "d6b9a20c6d77a750bfde",
+    "url": "./static/js/2.c81ba523.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.c81ba523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30427ed1fc08df9f18bc",
+    "url": "./static/js/main.229d65b0.chunk.js"
+  },
+  {
+    "revision": "585b2242d93ec53912e5",
+    "url": "./static/js/runtime-main.a56acb3b.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
